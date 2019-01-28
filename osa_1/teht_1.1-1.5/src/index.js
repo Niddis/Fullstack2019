0b35a82f694d6fd2a88a769cb5dcb5d3c3d7f2fd
Emma Ilkka <emma.ilkka@gmail.com>
2019-01-28 14:45:49 +0200

    kokeiltiin selkiyttää muotoiluja

--- osa_1/teht_1.1-1.5/src/index.js
+++ osa_1/teht_1.1-1.5/src/index.js
@@ -39,28 +39,28 @@ const App = () => {
 	const course = {
 		name: 'Half Stack -sovelluskehitys',
 		parts: [
-  		{
+			{
 				name: 'Reactin perusteet',
 				exercises: 10
 			},
-  		{
+			{
 				name: 'Tiedonvälitys propseilla',
 				exercises: 7
 			},
-  		{
+			{
 				name: 'Komponenttien tila',
 				exercises: 14
 			}
 		]
 	}
 
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts} />
-			<Total parts={course.parts}/>
-    </div>
-  )
+	return (
+		<div>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
